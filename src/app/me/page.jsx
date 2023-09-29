8d0ea556a144ad1df60aa0d4c1698e555201cff8
Prashant-2505 <shramaprashant2505@gmail.com>
2023-09-29 14:42:29 +0530

    check 4

---
+++ src/app/me/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-red-800 h[50vh] w-[50vh]'>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quis esse reprehenderit vero ex odit, minus dolorum quo, asperiores optio doloremque cum rerum, cupiditate nesciunt maxime! Modi minus nesciunt repellat!
+      </p>
+      <h1>Lorem, ipsum dolor.</h1>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae doloribus assumenda harum molestiae eius quod accusantium officiis, tempore eveniet suscipit a, excepturi non facilis, dolor magni perspiciatis minus ab! Ipsa?
+      </h1>
+    </div>
+  )
+}
+
+export default page
