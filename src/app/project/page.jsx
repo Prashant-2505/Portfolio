3a741120d32f9aebf0c05e4001e9ba60d2f29002
Prashant-2505 <shramaprashant2505@gmail.com>
2023-09-29 14:21:58 +0530

    deployment check

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -7,6 +7,8 @@ const page = () => {
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quis esse reprehenderit vero ex odit, minus dolorum quo, asperiores optio doloremque cum rerum, cupiditate nesciunt maxime! Modi minus nesciunt repellat!
       </p>
       <h1>Lorem, ipsum dolor.</h1>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae doloribus assumenda harum molestiae eius quod accusantium officiis, tempore eveniet suscipit a, excepturi non facilis, dolor magni perspiciatis minus ab! Ipsa?
+      </h1>
     </div>
   )
 }
