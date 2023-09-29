848a1b75db6925848b9d21fc66eb74adbaec93a1
Prashant-2505 <shramaprashant2505@gmail.com>
2023-09-29 14:45:33 +0530

    check 5

--- src/app/about/Page.jsx
+++ src/app/about/Page.jsx
@@ -1,16 +1,90 @@
+'use client'
+import AnimatedText from '@/components/AnimatedText'
+import Head from 'next/head'
 import React from 'react'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import arrow from '../../../public/icons/down_arrow.gif'
+import SKills from '@/components/SKills'
+import Experience from '@/components/Experience'
+import Education from '@/components/Education'
 
-const page = () => {
-  return (
-    <div className='bg-red-800 h[50vh] w-[50vh]'>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quis esse reprehenderit vero ex odit, minus dolorum quo, asperiores optio doloremque cum rerum, cupiditate nesciunt maxime! Modi minus nesciunt repellat!
-      </p>
-      <h1>Lorem, ipsum dolor.</h1>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae doloribus assumenda harum molestiae eius quod accusantium officiis, tempore eveniet suscipit a, excepturi non facilis, dolor magni perspiciatis minus ab! Ipsa?
-      </h1>
-    </div>
-  )
+
+
+
+const Page = () => {
+
+    const text = {
+        initial: {
+            opacity: 0
+        },
+        animate: {
+            opacity: 1,
+            transition: {
+                delay: 0.5,
+            }
+        }
+    }
+
+
+    // const skillsRef = useRef()
+    // const scrollToSkills = () => {
+    //     if (skillsRef.current) {
+    //         skillsRef.current.scrollIntoView({ behavior: 'smooth' });
+    //     }
+    // };
+
+
+
+    return (
+        <>
+            <Head>
+                <title>Prashant | About</title>
+                <meta name="description" content="--" />
+            </Head>
+
+            <main className='h-full w-full flex flex-col items-center justify-center p-5  mt-[7rem]'>
+                <AnimatedText
+                    className='!text-5xl text-center'
+                    text="Let Me Tell You More About Me"
+                />
+
+                <motion.p
+                    variants={text}
+                    initial="initial"
+                    animate="animate"
+                    className='!text-xl text-center font-normal mt-3 p-4' >
+                    Hello! I'm Prashant Sharma, a passionate web developer with a knack for turning ideas into interactive and efficient web applications. My journey in the world of web development has equipped me with a diverse skill set that spans both front-end and back-end technologies. I take pride in creating seamless digital experiences that not only look great but also function flawlessly.                </motion.p>
+
+                <motion.p
+                    variants={text}
+                    initial="initial"
+                    animate="animate"
+                    className='!text-xl text-center font-normal mt-3 p-4'>
+                    I have a strong affinity for problem-solving, and I enjoy tackling algorithmic challenges on platforms like LeetCode and GeeksforGeeks. Whether it's optimizing code or finding elegant solutions to complex problems, I'm always up for the challenge.
+                </motion.p>
+
+                <Image src={arrow}
+                    className=' h-[3rem] w-[3rem] mt-[5rem] hover:cursor-pointer'
+                />
+
+                {/* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+
+                <SKills  />
+
+                {/* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+
+
+                <Experience />
+
+                {/* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+
+
+                {/* <Education/> */}
+
+            </main>
+        </>
+    )
 }
 
-export default page
+export default Page
