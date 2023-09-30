5d17b9db9b8edd59b65013f30ad070d778767478
Prashant-2505 <shramaprashant2505@gmail.com>
2023-09-30 18:26:50 +0530

    fix 3

--- src/app/abouts/page.jsx
+++ src/app/abouts/page.jsx
@@ -1,90 +1,84 @@
 'use client'
-import AnimatedText from '@/components/AnimatedText'
-import Head from 'next/head'
-import React, {  useRef } from 'react'
-import { motion } from 'framer-motion'
+
 import Image from 'next/image'
-import arrow from '../../../public/icons/down_arrow.gif'
-import SKills from '@/components/SKills'
-import Experience from '@/components/Experience'
-import Education from '@/components/Education'
+import React from 'react'
+import demo from '../../../public/images/profile.png'
+import project1 from '../../../public/images/project1.png'
+import project2 from '../../../public/images/project2.png'
+
+
+import { BsGithub } from 'react-icons/bs'
+import Link from 'next/link'
 import { useThemeContext } from '../context/theme'
+const page = () => {
 
-const Page = () => {
-    const text = {
-        initial: {
-            opacity: 0
-        },
-        animate: {
-            opacity: 1,
-            transition: {
-                delay: 0.5,
-            }
-        }
-    }
+  const {theme} = useThemeContext()
 
-    const skillsRef = useRef()
-    const scrollToSkills = () => {
-        if (skillsRef.current) {
-            skillsRef.current.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
+  return (
+    <div className={` w-full mt-[6rem] text-center  justify-center items-center ${!theme ? 'bg-[#2c3e50] text-white':'text-[#2c3e50]'}`}>
+      <h1 className=' font-bold text-7xl inline-block px-32 leading-relaxed'>
+        Imagination Trumps Knowledge!
+      </h1>
+      <div className="grid h-full grid-cols-2 gap-9 place-items-center py-[5rem] ">
 
+        {/* project1 */}
+        <div className={`h-[100%] w-[40vw] flex flex-col   p-6 shadow-2xl rounded-md shadow-[#5c4f30] ${!theme ? 'bg-white text-[#2c3e50]':''}`}>
+          <Image className=' h-[40vh] w-full' src={project1} />
+          <h1 className='mt-5 font-bold text-2xl capitalize'>travellers</h1>
+          <p className='my-3 font-base text-left'>
 
-    const { theme } = useThemeContext()
-
-
-    return (
-        <>
-            <Head>
-                <title>Prashant | About</title>
-                <meta name="description" content="--" />
-            </Head>
-
-            <main className={`h-full w-full flex flex-col items-center justify-center p-5 mt-[6rem] ${!theme ? 'bg-[#2c3e50] text-white' : ''}`}>
-                <AnimatedText
-                    className={`!text-5xl text-center ${!theme ? '!text-white' : ''}`}
-                    text="Let Me Tell You More About Me"
-                />
-
-                <motion.p
-                    variants={text}
-                    initial="initial"
-                    animate="animate"
-                    className='!text-xl text-center font-normal mt-3 p-4' >
-                    Hello! I'm Prashant Sharma, a passionate web developer with a knack for turning ideas into interactive and efficient web applications. My journey in the world of web development has equipped me with a diverse skill set that spans both front-end and back-end technologies. I take pride in creating seamless digital experiences that not only look great but also function flawlessly.
-                </motion.p>
-
-                <motion.p
-                    variants={text}
-                    initial="initial"
-                    animate="animate"
-                    className='!text-xl text-center font-normal mt-3 p-4'>
-                    I have a strong affinity for problem-solving, and I enjoy tackling algorithmic challenges on platforms like LeetCode and GeeksforGeeks. Whether it's optimizing code or finding elegant solutions to complex problems, I'm always up for the challenge.
-                </motion.p>
-
-                <Image src={arrow}
-                    onClick={scrollToSkills}
-                    className={`h-[3rem] w-[3rem] mt-[5rem] hover:cursor-pointer ${!theme ? 'bg-white rounded-full' : ''}`}
-                />
-
-                {/* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */}
-
-                <SKills skillsRef={skillsRef} />
-
-                {/* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+            I have designed and developed a user-friendly web application aimed at travel enthusiasts seeking budget-friendly travel packages across various destinations in India. Leveraging cutting-edge technologies such as React.js, Firebase, and Google authorization, the platform offers a seamless booking experience.
+          </p>
+          <div className='text-left flex items-center space-x-10 my-4'>
+            <Link href={'https://travelers-theta.vercel.app/'} target='_blank'>            <button className=' bg-[#2c3e50] text-white px-12 py-3 rounded-md text-md font-semibold'>Visit Project</button>
+            </Link>
+            <Link href={'https://github.com/Prashant-2505/travelers'} target='_blank'>
+              <BsGithub className='text-4xl cursor-pointer' />
+            </Link>
+          </div>
+        </div>
 
 
-                <Experience />
 
-                {/* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+        {/* project 2 */}
+        <div className={`h-[100%] w-[40vw] flex flex-col   p-6 shadow-2xl rounded-md shadow-[#5c4f30] ${!theme ? 'bg-white text-[#2c3e50]':''}`}>
+          <Image className=' h-[40vh] w-full' src={project2} />
+          <h1 className='mt-5 font-bold text-2xl capitalize'>Vidiscape</h1>
+          <p className='my-3 font-base text-left'>
+            I developed VIDISCAPE," a YouTube clone web application meticulously crafted using React.js and Material-UI. This project aims to replicate the core functionalities of YouTube, allowing users to watch videos fetched from the YouTube API and presented through a visually appealing and user-friendly interface.          </p>
+          <div className='text-left flex items-center space-x-10 my-4'>
+            <Link href={'https://youtube-clone-six-vert.vercel.app/'} target='_blank'>            <button className=' bg-[#2c3e50] text-white px-12 py-3 rounded-md text-md font-semibold'>Visit Project</button>
+            </Link>
+            <Link href={'https://github.com/Prashant-2505/youtube-clone'} target='_blank'>
+              <BsGithub className='text-4xl cursor-pointer' />
+            </Link>
+          </div>
+        </div>
 
 
-                {/* <Education/> */}
+        {/* project 3 */}
+        <div className={`h-[100%] w-[40vw] flex flex-col   p-6 shadow-2xl rounded-md shadow-[#5c4f30] ${!theme ? 'bg-white text-[#2c3e50]':''}`}>
+          <Image className=' h-[40vh] w-full' src={demo} />
+          <h1 className='mt-5 font-bold text-2xl capitalize'>WearAble</h1>
+          <p className='my-3 font-base text-left'>
+            WearAble" is a dynamic ecommerce clothing web application, meticulously developed using a powerful technology stack comprising MongoDB, React, Node.js, and Express.js. This platform offers users a secure and seamless shopping experience for a diverse range of clothing items.
+          </p>
+          <div className='text-left flex items-center space-x-10 my-4'>
+            <Link href={'https://wearable.onrender.com/'} target='_blank'>            <button className=' bg-[#2c3e50] text-white px-12 py-3 rounded-md text-md font-semibold'>Visit Project</button>
+            </Link>
+            <Link href={'https://github.com/Prashant-2505/wearAble'} target='_blank'>
+              <BsGithub className='text-4xl cursor-pointer' />
+            </Link>
+          </div>
+        </div>
 
-            </main>
-        </>
-    )
+
+
+
+
+      </div>
+    </div>
+  )
 }
 
-export default Page;
+export default page
