f2e1a463298e0365ec43e46e6e06451ed722bb7f
Prashant-2505 <shramaprashant2505@gmail.com>
2023-09-30 18:31:16 +0530

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,18 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
 import Providers from './Provider'
+import { ThemeContextProvider } from './context/theme'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`w-full h-full inline-block z-0 bg-[#f7f1e3] `}>
-        <Providers>
+        <ThemeContextProvider>
           <Navbar />
           {children}
           <Footer />
-        </Providers>
+        </ThemeContextProvider>
 
       </body>
     </html>
