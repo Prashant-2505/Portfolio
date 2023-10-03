7cf513e146c2837a2aea8c336b3337bba1c71500
Prashant-2505 <shramaprashant2505@gmail.com>
2023-10-03 19:38:58 +0530

    ..

--- src/app/page.js
+++ src/app/page.js
@@ -12,13 +12,13 @@ export default function Home() {
   const { theme } = useThemeContext()
 
   return (
-    <main className={`flex h-[100vh] w-full  items-center justify-between mt-[5rem]  lg:flex-col lg:pt-9   ${theme ? 'bg-[#f7f1e3] text-[#2c3e50]' : 'bg-[#2c3e50] text-[#f7f1e3]'}`}>
-      <div className=" w-[50%] h-[80vh] md:w-[80%]  md:h-[50vh] lg:w-[100%] lg:h-[60%] flex justify-center items-center  md:inline-block">
+    <main className={`flex h-full w-full  items-center justify-between mt-[5rem]  lg:flex-col lg:pt-9   ${theme ? 'bg-[#f7f1e3] text-[#2c3e50]' : 'bg-[#2c3e50] text-[#f7f1e3]'}`}>
+      <div className=" w-[50%] h-full md:w-[80%]  md:h-[50vh] lg:w-[100%] lg:h-[60%] flex justify-center items-center  md:inline-block">
         <Image
           priority={true}
           className='h-[70%] w-[80%] 
            sm:h-[40vh] sm:mb-12
-           md:w-full 
+           md:w-full md:h-[50vh] 
            sm:w-full 
            ' src={profile} alt='Prashant' />
       </div>
@@ -43,7 +43,7 @@ export default function Home() {
         `}>
           As a skilled full-stack developer, I am to turning ideas into innovative web applications, Explore my latest projects , showcasing my expertise in React.js , Next.js and web development
         </p>
-        <div className=" flex items-center self-start mt-2 lg:self-center">
+        <div className=" flex items-center self-start my-9 lg:self-center">
           <Link
             className={`flex  items-center gap-2 border-[2px] ${theme ? 'bg-[#2c3e50] text-white' : 'bg-[#f7f1e3] text-[#2c3e50]'} p-2.5 px-6 rounded-lg text-lg font-semibold ${theme ? 'hover:bg-white hover:text-[#2c3e50] transition 0.3s ease-in hover:border-[2px]' : 'hover:bg-[#2c3e50] hover:text-white transition 0.3s ease-in border-[2px] border-[#2c3e50] hover:border-[2px]'} `}
             download={true}
