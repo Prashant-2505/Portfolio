1266d72458569cc9742ca17a66cdb9ad87378ad1
Prashant-2505 <shramaprashant2505@gmail.com>
2024-03-07 23:26:49 +0530

    ..

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
 import { ThemeContextProvider } from './context/theme'
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
